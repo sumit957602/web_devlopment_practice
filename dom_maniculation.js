a1b4efde5a20027b7076d0357c96843f082b6832
Sumit Kumar <106050212+sumit957602@users.noreply.github.com>
2023-08-16 19:52:42 +0530

    Update dom_maniculation.js

--- dom_maniculation.js
+++ dom_maniculation.js
@@ -1,3 +1,4 @@
-const mybody = document.getElementsByClassName('a1')
+const mybody = document.getElementsByClassName('a1');
+const a2 = document.getElementById('a2');
 console.log(a2);
 mybody.style 

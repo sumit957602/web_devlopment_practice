981ca4a0f63a6d1f91a53871a5c8d84ff1976bb4
Sumit Kumar <106050212+sumit957602@users.noreply.github.com>
2024-07-26 08:27:04 +0530

    React practice 2 started

---
+++ React-practice2/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  let styles = {
+    color: 'red',
+    fontSize: '30px',
+  };
+  return (
+    <>
+      <h1 style={styles}>Hello World</h1>
+    </>
+  )
+}
+
+export default App
